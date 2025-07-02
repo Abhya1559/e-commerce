a03033b9ec0b904edfa16c176a1ec06dd54df0bc
Abhya1559 <abhya1559@gmail.com>
2025-07-02 22:01:12 +0530

    Add Prettier configuration and integrate button component in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
-      <div>
-        <h1 className="text-2xl font-bold">Hello This is beginning</h1>
+    <div className="flex  flex-col item-center justify-center p-24">
+      <div className="flex flex-col items-center justify-center space-y-8">
+        <h1 className="text-center font-bold text-3xl">
+          Hello this is beginning of my e-commerce app
+        </h1>
+        <Button variant={"outline"} className="cursor-pointer">
+          Hello I am working properly
+        </Button>
       </div>
     </div>
   );
